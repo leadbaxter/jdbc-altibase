de5cedc3a4452fbf162732304b8f9f378c1f6fd3
Brian Jackson <brianj2@gmail.com>
2015-02-18 15:44:24 -0600

    updated altibase jar file to version 6.3.1.3.1

--- lib/jdbc/altibase.rb
+++ lib/jdbc/altibase.rb
@@ -1,7 +1,7 @@
 require 'jdbc/altibase/version'
 
 if RUBY_PLATFORM =~ /java/
-  require 'jdbc/altibase.jar'
+  require "jdbc/altibase_#{::Jdbc::Altibase.driver_version}.jar"
 else
   warn 'jdbc-altibase gem is only for use with JRuby'
 end
--- lib/jdbc/altibase/version.rb
+++ lib/jdbc/altibase/version.rb
@@ -1,5 +1,11 @@
 module Jdbc
   module Altibase
-    VERSION = '0.0.1'
+    VERSION = '0.0.1'.freeze
+    DRIVER_VERSION = '6.3.1.3.1'.freeze
+
+    def self.driver_version
+      DRIVER_VERSION
+    end
+
   end
 end
